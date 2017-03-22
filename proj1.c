43e9ac1a397e7d0e1c8d03fb4cbba8bd9f413de1
mamfbr <mamfbr@gmail.com>
2017-03-22 19:27:10 -0300

    adicionada e levemente testada funcao de leitura do arquivo

--- proj1.c
+++ proj1.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+    int id;
+    struct Adj *prox;
+} Adj;
 
 typedef struct 
 {
     int id;
     char nome[30];
     char matricula[10];
-    struct Aluno *prox;
+    struct Adj *adj;
     
 } Aluno;
 
+
+
 /*define numero de alunos*/
 #define NUM_ALUNOS 39
+/*define tamanho max de cada linha do arquivo alunos.txt*/
+#define TAM_LINHA 100
 
 /*CABECALHO DE FUNCOES*/
 int povoaLista (Aluno **);
+void pushAdj(Aluno *, int);
 
 /*FUNCAO MAIN*/
 int main () {
@@ -33,40 +45,86 @@ int main () {
 
 }
 
-#define TAM_LINHA 100
+
 /*Percorre o arquivo alunos.txt e povoa a lista de adjacencia*/
 int povoaLista(Aluno **lista) {
 
     /*declara e inicializa variaveis a serem utilizadas*/
     FILE *arqAlunos = NULL;
     char linhaAtual[TAM_LINHA];
-    Aluno aluno;
     int i = 0;
+    int idAdj = 0;
+    int offsetLinha = 0;
+    
     
     
     /*Abre arquivo com nomes e matriculas, em modo leitura*/
     arqAlunos = fopen("alunos.txt", "r");
     if (arqAlunos == NULL) {
         printf("\nERRO: Arquivo alunos.txt nao encontrado\n");
-        return 1;
+        return -1;
     }
     
     /*
     percorre o arquivo alunos.txt
-    vai de linha em linha, supondo formato id#nome#matricula\n
-    percorre ate chegar a NUM_ALUNOS ou EOF (equivalente a fscanf ler diferente de 3)
+    vai de linha em linha, colocando em uma string, supondo formato nome#matricula#idAdj1#idAdj2#...#idAdjn#\n
+    percorre ate chegar a NUM_ALUNOS ou EOF (feof == 0)
     a cada iteracao, aloca espaco para novo vertice e atribui a ele os valores lidos, e depois adiciona suas adjacencias
+    supoe id do aluno a partir da ordem de entrada
     
-    "%d#%[a-zA-Z ]#%9s\n", &(aluno.id), aluno.nome, aluno.matricula
     */
-    for(i = 0;(fgets(linhaAtual, TAM_LINHA, arqAlunos) != NULL) || i < NUM_ALUNOS; i++) {
+    /*
+    no for, primeira condicao de saida verifica se houve erro
+    segunda condicao se ja leu todos os alunos registrados
+    terceira se chegou no final do arquivo
+    */
+    for(i = 0;(/*!feof(arqAlunos) &&*/ (i < NUM_ALUNOS) && fgets(linhaAtual, TAM_LINHA, arqAlunos) != NULL); i++) {
+        
+        /*declara o offselinha, pois o arquivo sera lido linha a linha, e esta variavel ira funcionar como forma de apontar para o caracter a ser lido*/
+        offsetLinha = 0;
+    
         /*Aloca */
-        lista[i] = malloc(sizeof(Aluno));    
-        printf("id=%d\n",aluno.id);
-        printf("nome=%s\n", aluno.nome);
-        printf("matricula=%s\n", aluno.matricula);
+        lista[i] = malloc(sizeof(Aluno));
+        /*coloca o id do aluno*/
+        lista[i]->id = i+1;
+        sscanf(linhaAtual, "%[a-zA-Z ]#%9[0-9][^#]#", lista[i]->nome, lista[i]->matricula);
+        /*calcula o novo offset, dado que foi lido nome e matricula*/
+        
+        printf("id=%d\n",lista[i]->id);
+        printf("nome=%s\n", lista[i]->nome);
+        printf("matricula=%s\n", lista[i]->matricula);
+        
+        offsetLinha = strlen(lista[i]->nome) + strlen(lista[i]->matricula) + 2;
+        
+        /*Le os adjacentes, e sai povoando a lista de adjacencia*/
+        while(sscanf(&linhaAtual[offsetLinha],"%d#", &idAdj) == 1) {
+           /*Coloca o novo vertice no inicio da lista de adj*/
+           
+           pushAdj(lista[i], idAdj);
+           /*anda na string para o prox valor*/
+           for(;linhaAtual[offsetLinha] != '#' || strlen(&linhaAtual[offsetLinha]) == 0; offsetLinha++);
+           offsetLinha++;
+        }
+        printf("\n");
+        
+
+        
         
     }
+    return i;
+}
+
+void pushAdj(Aluno *aluno, int id) {
+
+    /*aloca espaco para novo vertice*/
+    Adj *adjTemp = malloc(sizeof(Adj));
     
-    return 0;
+    /*coloca valor do id*/
+    adjTemp->id = id;
+    printf("->%d", id);
+    /*aponta o prox do novo vertice para o primeiro elemento da lista de adjacencia do aluno*/
+    adjTemp->prox = aluno->adj;
+    /*aponta primeiro elemento da lista de adj do aluno para novo vertice*/
+    aluno->adj = adjTemp;
+    
 }
